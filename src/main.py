5cd90cf99b525759af7c5a5cc53424b0ddf1140b
Eduardo Angulo <edangulo@uninorte.edu.co>
2023-03-16 17:26:07 -0500

    Added company creation to main file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 # Main File
+from company.company import Company
 
 def main() -> None:
-    pass
+    nit = input('Digite el nit: ')
+    name = input('Digite el nombre: ')
+    num_employee = int(input('Digite la cantidad de empleados: '))
+    company1 = Company(nit, name, num_employee)
 
 
 if __name__ == '__main__':
